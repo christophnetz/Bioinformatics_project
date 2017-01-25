59be28902bcc691e5b4210f20bc3dfe6a8f56f6c
C.Netz@student.rug.nl <s3268179@peregrine.hpc.rug.nl>
2017-01-25 16:23:26 +0100

    loops added

--- masterscript2.sh
+++ masterscript2.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
 
+#Christoph Netz, s3268179, 25-1-2017
+
+#creates text files from csv table 
+#containing sequence labels (labels.txt) 
+#and accession numbers (COI, CAD, DDC, EF1a, Per, Wgl, $_Acc.txt
 extr_data.py
 
-
+#create label file for the respective sequence by extracting non-missing sequence labels
 grep -v "\-\",$" labels.txt > Wgl_labels.txt
 grep -v "\-_[^_]*$" labels.txt > Per_labels.txt
 grep -v "\-_[^_]*_[^_]*$" labels.txt > EF1a_labels.txt
@@ -11,36 +16,40 @@ grep -v "\-_[^_]*_[^_]*_[^_]*_[^_]*$" labels.txt > CAD_labels.txt
 grep -v "\-_[^_]*_[^_]*_[^_]*_[^_]*_[^_]*$" labels.txt > COI_labels.txt
 
 
-grep -v "\-\".*$" COI_Acc.txt > 2COI_Acc.txt
-grep -v "\-\".*$" CAD_Acc.txt > 2CAD_Acc.txt
-grep -v "\-\".*$" DDC_Acc.txt > 2DDC_Acc.txt
-grep -v "\-\".*$" EF1a_Acc.txt > 2EF1a_Acc.txt
-grep -v "\-\".*$" Per_Acc.txt > 2Per_Acc.txt
-grep -v "\-\".*$" Wgl_Acc.txt > 2Wgl_Acc.txt
+#remove missing accession numbers from accession number files
+#keep only modified version
+for file in *Acc.txt;
+do
+grep -v "\-\".*$" $file > 2$file
+rm $file
+done
 
-
-tr -d '\n' < 2COI_Acc.txt > 3COI_Acc.txt
-tr -d '\n' < 2CAD_Acc.txt > 3CAD_Acc.txt
-tr -d '\n' < 2DDC_Acc.txt > 3DDC_Acc.txt
-tr -d '\n' < 2EF1a_Acc.txt > 3EF1a_Acc.txt
-tr -d '\n' < 2Per_Acc.txt > 3Per_Acc.txt
-tr -d '\n' < 2Wgl_Acc.txt > 3Wgl_Acc.txt
-
-#tr -d '\n' < *.txt > $.txt
-
-tr -d '\n' < COI_labels.txt > COI_labels2.txt
-tr -d '\n' < CAD_labels.txt > CAD_labels2.txt
-tr -d '\n' < DDC_labels.txt > DDC_labels2.txt
-tr -d '\n' < EF1a_labels.txt > EF1a_labels2.txt
-tr -d '\n' < Per_labels.txt > Per_labels2.txt
-tr -d '\n' < Wgl_labels.txt > Wgl_labels2.txt
+#Remove line endings in all text files
+#keep only modified files
+for file in *.txt; 
+do
+tr -d '\n' < $file > 3$file
+rm $file
+done
 
 
 #Removing commata at line end
 sed -ie 's/,$//g' *.txt
 
+#Remove *.txte files that are produced by 'sed'
+rm *.txte
+
 #R_seqimport.r
 
+
+
+#Alignment
+
+for file in *.fasta;
+do
+"/usr/bin/mafft"  --globalpair --maxiterate 1000 --reorder \
+--adjustdirectionaccurately --phylipout --namelength 200 "$file" > "al_$file.phy"
+done
 
 #"/usr/bin/mafft"  --globalpair --maxiterate 1000 --reorder --adjustdirectionaccurately "COI.fasta"  > "al_COI.fasta"
 #"/usr/bin/mafft"  --globalpair --maxiterate 1000 --reorder --adjustdirectionaccurately "CAD.fasta"  > "al_CAD.fasta"
